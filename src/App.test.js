a573bfb7c58859998ac19e061bb6f2cec4ec37dd
Zviki_Zaks <zvikizax@gmail.com>
2022-07-28 15:48:47 +0300

    add first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
 import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom/client';
+import { act } from 'react-dom/test-utils';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App-jest', () => {
+  let container = null;
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
+  });
+
+  it('rendering app component', () => {
+    act(() => {
+      ReactDOM.createRoot(container).render(<App />);
+    })
+    const appEl = document.querySelector('.todo-app');
+    expect(appEl).toBeInTheDocument();
+  });
+});
+
+describe('App-RTL', () => {
+
+  it('rendering app component', () => {
+    render(<App />);
+    expect(screen.getByText(/todo app/i)).toBeInTheDocument();
+    expect(screen.getByRole('heading')).toHaveTextContent(/todo app/i);
+  });
 });
